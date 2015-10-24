e80ead1f14a149dc464dd8e77e46ffd06c9e8484
Glassmaker <JeanGlassmaker@mail.com>
2015-10-24 23:57:15 +0300

    Adds configuration basics

--- src/main/java/com/teammetallurgy/metallurgycm/handler/ConfigHandler.java
+++ src/main/java/com/teammetallurgy/metallurgycm/handler/ConfigHandler.java
@@ -7,6 +7,22 @@ public class ConfigHandler
 
     private static Configuration config;
 
+    private static String[] abstractorTypes = { "Prometheum", "Deep Iron", "Black Steel", "Oureclase", "Mithril", "Haderoth", "Orichalcum", "Adamantine", "Atlarus", "Tartarite" };
+    public static double[] abstractorSpeedMultipliers = {};
+    public static double[] abstractorFuelEfficiencyMultipliers = {};
+
+    private static String[] crusherTypes = { "Stone", "Copper", "Bronze", "Iron", "Steel" };
+    public static double[] crusherSpeedMultipliers = {};
+    public static double[] crusherFuelEfficiencyMultipliers = {};
+
+    private static String[] furnaceTypes = { "Copper", "Bronze", "Iron", "Steel" };
+    public static double[] furnaceSpeedMultipliers = {};
+    public static double[] furnaceFuelEfficiencyMultipliers = {};
+
+    private static String[] smelterTypes = { "Ignatius", "Shadow Iron", "Shadow Steel", "Vyroxeres", "Inolashite", "Kalendrite", "Vulcanite", "Sanguinite" };
+    public static double[] smelterSpeedMultipliers = {};
+    public static double[] smelterFuelEfficiencyMultipliers = {};
+
     public static void setConfig(Configuration configuration)
     {
         config = configuration;
@@ -16,6 +32,9 @@ public class ConfigHandler
     {
         config.load();
 
-        if (config.hasChanged()) config.save();
+        if (config.hasChanged())
+        {
+            config.save();
+        }
     }
 }
